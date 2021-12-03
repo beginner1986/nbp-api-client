b936c9aefca78b7af6014524f9268e7e24b8f04e
beginner1986 <beginner1986@gmail.com>
2021-12-03 22:14:04 +0100

    Data fetching in TabA implemented. Data presentation pending.

--- src/components/TabA.js
+++ src/components/TabA.js
@@ -1,5 +1,34 @@
+import {useEffect, useState} from "react";
+
 export default function TabA(props) {
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [error, setError] = useState(null);
+    const [date, setDate] = useState("");
+    const [rates, setRates] = useState([]);
+
+    useEffect(() => {
+        const url = "http://api.nbp.pl/api/exchangerates/tables/a/";
+
+        fetch(url)
+            .then(res => res.json())
+            .then((data) => {
+                setIsLoaded(true);
+                setDate(data.at(0).effectiveDate);
+                setRates(data.at(0).rates);
+                },
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            );
+        }, []
+    );
+
+    let components = rates.map((rate) => { return (<div key={rate.code}>{rate.mid}</div>)});
+
     return (
-        <div>Tabela A</div>
+        <div>
+            {components}
+        </div>
     );
 }
