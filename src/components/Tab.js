a30e87b05295678c18b4c3e9a41a36f4b70d98c9
beginner1986 <beginner1986@gmail.com>
2021-12-04 21:49:55 +0100

    Tab component code structurized.

--- src/components/Tab.js
+++ src/components/Tab.js
@@ -33,52 +33,6 @@ export default function Tab(props) {
         }, [props.tab]
     );
 
-    let headers = (
-        <tr>
-            <th>Lp.</th>
-            <th>Kod</th>
-            <th>Waluta</th>
-            {
-                function() {
-                    if(tabC) {
-                        return (
-                            <>
-                                <th>Średni kurs zakupu</th>
-                                <th>Średni kurs sprzedaży</th>
-                            </>
-                        );
-                    } else {
-                        return <th>Kurs średni</th>;
-                    }
-                }()
-            }
-
-        </tr>
-    );
-
-    let components = rates.map((rate, index) => {
-        return (
-            <tr key={rate.code}>
-                <td>{index + 1}</td>
-                <td>{rate.code}</td>
-                <td>{rate.currency}</td>
-                {
-                    function() {
-                        if(tabC) {
-                            return (
-                                <>
-                                    <td>{rate.ask}</td>
-                                    <td>{rate.bid}</td>
-                                </>
-                            );
-                        } else {
-                            return <td>{rate.mid}</td>;
-                        }
-                    }()
-                }
-            </tr>);
-    });
-
     if(error) {
         const errorMessage = `Nie można pobrać tabeli ${props.tab.toUpperCase()}...`;
         return <Error error={errorMessage}/>
@@ -86,17 +40,67 @@ export default function Tab(props) {
         return <Spinner animation="border"/>
     } else {
         return (
-            <div className="tab-a">
+            <div className="tab">
                 <h2>Notowania z dnia {date}:</h2>
                 <Table striped bordered hover>
                     <thead>
-                        {headers}
+                        {generateTableHeaders()}
                     </thead>
                     <tbody>
-                        {components}
+                        {generateTableBody()}
                     </tbody>
                 </Table>
             </div>
         );
+    }
+
+    function generateTableHeaders() {
+        return (
+            <tr>
+                <th>Lp.</th>
+                <th>Kod</th>
+                <th>Waluta</th>
+                {
+                    function() {
+                        if(tabC) {
+                            return (
+                                <>
+                                    <th>Średni kurs zakupu</th>
+                                    <th>Średni kurs sprzedaży</th>
+                                </>
+                            );
+                        } else {
+                            return <th>Kurs średni</th>;
+                        }
+                    }()
+                }
+
+            </tr>
+        );
+    }
+
+    function generateTableBody() {
+        return rates.map((rate, index) => {
+            return (
+                <tr key={rate.code}>
+                    <td>{index + 1}</td>
+                    <td>{rate.code}</td>
+                    <td>{rate.currency}</td>
+                    {
+                        function() {
+                            if(tabC) {
+                                return (
+                                    <>
+                                        <td>{rate.ask}</td>
+                                        <td>{rate.bid}</td>
+                                    </>
+                                );
+                            } else {
+                                return <td>{rate.mid}</td>;
+                            }
+                        }()
+                    }
+                </tr>);
+        });
     }
 }
