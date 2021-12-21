d00fc96764564e9c00d9ab6da337ed27662c7fe8
beginner1986 <beginner1986@gmail.com>
2021-12-21 20:49:12 +0100

    About page content and styling added.

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,20 @@
 export default function About() {
     return (
-        <div>About</div>
+        <div>
+            <h2 className="text-primary text">Klient REST API Narodowego Banku Polskiego napisany jako ćwiczenie w ramach nauki React'a.</h2>
+            <br/>
+            <div>
+                <h3>Wykorzystane biblioteki:</h3>
+                <ul>
+                    <li><a href="https://reactjs.org/">React</a></li>
+                    <li><a href="https://reactrouter.com/">React Router</a></li>
+                    <li><a href="https://react-bootstrap.github.io/">React Bootstrap</a></li>
+                    <li><a href="https://getbootstrap.com/">Bootstrap</a></li>
+                    <li><a href="https://www.npmjs.com/package/react-router-bootstrap/">React Router Boostrap</a></li>
+                    <li><a href="https://fontawesome.com/">Font Awsome</a></li>
+                    <li><a href="https://animate.style/">Animate.css</a></li>
+                </ul>
+            </div>
+        </div>
     );
 }
