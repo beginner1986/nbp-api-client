a2ebdae316847c054db08e8e0248345f19ed677d
beginner1986 <beginner1986@gmail.com>
2022-01-01 22:46:09 +0100

    Rate rendering test added.

---
+++ src/components/cards/Rate.test.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Rate from "./Rate";
+
+it("renders without crushing", () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Rate/>, div);
+})
